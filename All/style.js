284a2cdabf383539d6754fd13c6755f4845c2d79
subham <subhamjaiswalamell@gmail.com>
2021-08-17 14:56:13 +0530

    First Copy

---
+++ All/style.js
@@ -0,0 +1,5 @@
+const firstExample = document.querySelector("#check");
+const secondExample = document.querySelector("#check1");
+
+console.log(firstExample.parentElement.innerText);
+console.log(secondExample.parentElement.innerText);
